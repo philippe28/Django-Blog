f7119ac75f6ad9e70984c4400d00c4d97390076f
Philippe Castro <phadc28@gmail.com>
2017-10-02 20:46:19 -0300

    add imagem 4

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -24,3 +25,10 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
