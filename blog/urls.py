22caa9b0af9eaf836a4614e95ef1a51b8b2f3dd7
alonsomachado <alonsomachado2@hotmail.com>
2017-10-02 19:08:06 -0400

    analitycs
    teste analytics 5

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_lista, name='post_lista'),
     url(r'^post/(?P<slug>[\w_-]+)/$', views.post, name='post_detalhe'),
+	url(r'^/contato', views.post, name='contato'),
 ]
