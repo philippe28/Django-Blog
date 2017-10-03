bbb02ba0bde861b2eb6873214aeafdec9319d4ae
Philippe Castro <phadc28@gmail.com>
2017-10-02 21:10:29 -0300

    add imagem 6

--- core/urls.py
+++ core/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(
+    + static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
