f9b89e889db00c95e068d0fc3e2dfe2de9f6dd35
alonsomachado <alonsomachado2@hotmail.com>
2017-10-06 14:00:36 -0400

    a
    a

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 import datetime
 from django.shortcuts import render
 from django.utils import timezone
+from django.template import loader, Context
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
 # Create your views here.
 
 from .models import Publicacao, Contato
@@ -58,11 +60,8 @@ def contato(request):
 
 def tabela(request):
     tabela = Contato.objects.order_by('nome')
-
+	
     return render(request, 'blog/tabela.html',{'tabela': tabela})
-
-
-
 
 def landing(request):
 
@@ -99,6 +98,24 @@ def softskills(request):
         contato.save()
 
     return render(request, 'blog/softskills.html',)
+	
+def metricaspararh(request): 
+
+    form = ContatoForm(request.POST or None)
+    if form.is_valid():
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        contato = form.save(commit=False)
+        contato.nome = request.POST['nome'] +" "+request.POST['sobrenome']
+        contato.email = request.POST['email']
+        contato.ip = ip
+        contato.save() 
+
+    return render(request, 'blog/metricaspararh.html',)
 	
 
 def send_email(request):
